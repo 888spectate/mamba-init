b78229811fcc50a381e406d9bc88b6424a052bcd
Robert Oliveira <Robert.Oliveira@dedsert.com>
2015-07-22 13:02:55 +0100

    Newest version using twisted to start instead of mamba-admin

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Robert Oliveira <olivecoder@gmail.com>
 
 ### BEGIN INIT INFO
@@ -12,81 +12,149 @@
 #                    in the same directory structure as the init script.
 ### END INIT INFO
 
-get_mamba_app_name() {
-    cat $CONFIG |
-    python -c "import json, sys; print json.load(sys.stdin)['name']"
-}
-
-RUN_AS=ec2-user
+RUN_AS=dedsert
+APPNAME=$(basename $0)
 WORKDIR=$(dirname $(dirname $(readlink -e $0)))
-PIDFILE=$WORKDIR/twistd.pid
+PIDDIR=/var/run/$RUN_AS
+PIDFILE=$PIDDIR/$APPNAME.pid
 CONFIG=$WORKDIR/config/application.json
-APPNAME=$(get_mamba_app_name)
 CMD=mamba-admin
+OPTS="--syslog "
+LOCKDIR=/var/lock/dedsert
 
-. /etc/default/$APPNAME
+exec > >(tee >(logger -t $APPNAME))
+exec 2> >(tee >(logger -t $APPNAME -p user.err) >&2)
 
-log_action_msg() {
-    echo $*
-}
+# imports
+[ -f /etc/default/$APPNAME ] && source /etc/default/$APPNAME
+# [ -f /etc/init.d/functions ] && . /etc/init.d/functions
+# [ -f /lib/lsb/init-functions ] && . /lib/lsb/init-functions
 
-[ -f /etc/init.d/functions ] && . /etc/init.d/functions
-[ -f /lib/lsb/init-functions ] && . /lib/lsb/init-functions
-
-find_first_env() {
-    VENV_DIR=$1
-    find -L $VENV_DIR -name '*env' -type d -executable \
-	-exec find {}/bin -name activate \; \
-	-quit 2>/dev/null
+die() {
+    echo $@ >&2
+    exit 1
 }
 
 get_virtualenv() {
-    ACTIVATE=$(find_first_env "$WORKDIR")
-    if [ "$ACTIVATE" = "" ]; then
-	ACTIVATE=$(find_first_env $(dirname(WORKDIR)))
-	if [ "$ACTIVATE" = "" ]; then
-	    echo ERROR: virtualenv not found
-	    exit 1
-	fi
+    path_list="$WORKDIR $WORKDIR/.."
+    activate_cmd=$(
+        find -L $path_list -iname '*env' -o -iname '*environment' -type d -executable \
+             -exec find {}/bin -name activate \; -quit
+    )
+    export VIRTUAL_ENV=$(dirname $(dirname $activate_cmd))
+    [ -n $VIRTUAL_ENV ]
+}
+
+is_running() {
+    [ -f $PIDFILE ] || return 1
+    [ -r $PIDFILE ] || die Cannot read $PIDFILE
+    ps $(cat $PIDFILE) &>/dev/null
+}
+
+create_dir() {
+    sudo mkdir -p $1 || die Needs to run as SU the first time to create dirs
+    sudo chown dedsert:dedsert $1 || die
+    sudo chmod g+wxs $1 || die
+}
+
+init_lock() {
+    [ -d $LOCKDIR ] || create_dir $LOCKDIR
+    LOCK=$LOCKDIR/$APPNAME
+    if ! lockfile -r 0 $LOCK &>/dev/null; then
+       [ -f $LOCK ] && die Already running || die Cannot write to $LOCK
+    fi
+    trap "rm -f $LOCK" 0
+}
+
+run_as() {
+    if (( $(id -u $RUN_AS) == $(id -u) )); then
+        return 0
+    else
+        sudo -u $RUN_AS true ||
+            die You need to be run me as the $RUN_AS user
+		echo Running as the $RUN_AS user
+        sudo -i -u $RUN_AS $@
+        exit $?
     fi
 }
 
-mamba_admin_exec() {
-    get_virtualenv
-    sudo -u $RUN_AS sh << EOF
-        cd $WORKDIR
-        . $ACTIVATE
-        $CMD $*
-EOF
-}
-
 do_start() {
-    mamba_admin_exec start
+    if is_running; then
+        echo Already running
+        return 0
+    fi
+    echo Starting...
+    [ -n "$VIRTUAL_ENV" ] || get_virtualenv || die failed to get a virtualenv
+    [ -d $PIDDIR ] || create_dir $PIDDIR
+    uid=$(id -u $RUN_AS)
+    gid=$(id -g $RUN_AS)
+    opts="-d $WORKDIR --prefix=$APPNAME --pidfile=$PIDFILE \
+          --syslog --prefix=$APPNAME -u $uid -g $gid $OPTS --umask 0022"
+    cd $WORKDIR
+    $VIRTUAL_ENV/bin/twistd $opts $APPNAME ||
+        die Error: Check the application log
+    sleep 3
+    is_running || die Failed to start. Unknown reason.
+    echo Started successfuly
+    exit 0
 }
 
 do_stop() {
-    mamba_admin_exec stop
-    [ -f "$PIDFILE" ] && rm $PIDFILE
-    find "$WORKDIR" -name '*.pyc' -exec rm {} \;
+    if ! is_running; then
+        echo Not running
+        return 0
+    fi
+    echo Stopping...
+    kill $(cat $PIDFILE) || return 1
+    sleep 3
+    is_running && die Failed to stop! try $0 kill
+    echo Stopped successfully
+    return 0
 }
+
+do_forcestop() {
+    do_stop && return 0
+    echo Failed to stop. Sending SIGKILL...
+    kill -9 $(cat $PIDFILE) || die
+    sleep 3
+    is_running ||
+        (echo Stopped successfully | exit 0) &&
+        die Failed to kill!
+}
+
+# this should run as su at least once to create the directories below
+[ -d $LOCKDIR ] || create_dir $LOCKDIR
+[ -d $PIDDIR ] || create_dir $PIDDIR
+
+# prevent mamba-admin start
+if [ ! -f $WORKDIR/twistd.pid ]; then
+    touch $WORKDIR/twistd.pid
+    sudo chmod 000 $WORKDIR/twistd.pid
+fi
+
+# rerun itself as the RUN_AS user
+id $RUN_AS &>/dev/null || die User $RUN_AS doesnt exist
+run_as $0 $@
+
+# In the end, there can be only one
+init_lock
 
 case "$1" in
     start)
-        log_action_msg "Starting $NAME"
         do_start
         ;;
     stop)
-        log_action_msg "Stopping $NAME"
         do_stop
         ;;
+    forcestop)
+        do_forcestop
+        ;;
     restart)
-        log_action_msg "Restarting $NAME"
         do_stop
         do_start
         ;;
     *)
-        log_action_msg "Usage: $0 {start|stop|restart}"
-        exit 2
+        die "Usage: $0 {start|stop|forcestop|restart}"
         ;;
 esac
 
--- install.sh
+++ install.sh
@@ -1,73 +1,44 @@
-#!/bin/sh
-# Radu Aldea
+#!/bin/bash
+# Robert Oliveira <olivecoder@gmail.com>
 # run as sudo
-
-
-SCRIPTSDIR=$(dirname $(readlink -e $0))
-INIT_DIR=${0%/*}
-
-replace_app_user() {
-    sed -i "/RUN_AS=/d" $SCRIPTSDIR/default;
-    echo "RUN_AS=$USER" >> $SCRIPTSDIR/default;
-}
-
-multiple_mamba_app() {
-    for i in `ls $DIR/*/config/application.json`; do
-        APPDIR=${i%/*/*} ;
-        CONFIG=$APPDIR/config/application.json
-        APPNAME=$(get_mamba_app_name)
-        cp $SCRIPTSDIR $APPDIR -ar
-        ln -sf $APPDIR/$INIT_DIR/init.sh /home/raldea/dedsert_applications/etc/init.d/$APPNAME
-        cp $APPDIR/$INIT_DIR/default /home/raldea/dedsert_applications/etc/default/$APPNAME
-    done
-    exit 0
-}
-
-different_mamba_dir() {
-    if [ -d "$DIR" ]; then
-        CONFIG=$DIR/config/application.json
-        APPNAME=$(get_mamba_app_name)
-        cp $SCRIPTSDIR $DIR -ar
-        ln -sf $DIR/$INIT_DIR/init.sh /home/raldea/dedsert_applications/etc/init.d/$APPNAME
-        cp $DIR/$INIT_DIR/default /home/raldea/dedsert_applications/etc/default/$APPNAME
-    fi
-    exit 0
-}
-
 
 get_mamba_app_name() {
     cat $CONFIG |
     python -c "import json, sys; print json.load(sys.stdin)['name']"
 }
 
-simple_install() {
-    CONFIG=$SCRIPTSDIR/../config/application.json
-    APPNAME=$(get_mamba_app_name)
-    ln -sf $SCRIPTSDIR/init.sh /home/raldea/dedsert_applications/etc/init.d/$APPNAME
-    cp $SCRIPTSDIR/default /home/raldea/dedsert_applications/etc/default/$APPNAME
+SCRIPTSDIR=$(dirname $(readlink -e $0))
+CONFIG=$SCRIPTSDIR/../config/application.json
+APPNAME=$(get_mamba_app_name)
+DEFAULT=/etc/default/$APPNAME
+WORKDIR=$SCRIPTSDIR/..
+
+die() {
+    echo ERROR: $@ >&2
+    exit 1
 }
 
-rm -rf $SCRIPTSDIR/.git
-if [ -z "$*" ]; then
-    $(simple_install)
-fi
-while getopts u:m:d: option; do
-    case "${option}" in
-        u)
-            USER=${OPTARG}
-            $(replace_app_user)
-            ;;
-        m)
-            DIR=${OPTARG}
-            $(multiple_mamba_app)
-            ;;
-        d)
-            DIR=${OPTARG}
-            $(different_mamba_dir)
-            ;;
-        *)
-            $(simple_install)
-            ;;
-    esac
-done
-shift $((OPTIND-1))
+create_dir() {
+    sudo mkdir -p $1 || die Needs to run as SU the first time to create dirs
+    sudo chown dedsert:dedsert $1 || die
+    sudo chmod g+wxs $1 || die
+}
+
+[ "$(id -u)" == "0" ] || die Run me as root/sudo
+
+ln -sf $SCRIPTSDIR/init.sh /etc/init.d/$APPNAME
+[ -f $DEFAULT ] || (
+	echo RUN_AS=dedsert | sudo tee $DEFAULT >/dev/null
+	sudo chown dedsert:dedsert $DEFAULT
+)
+
+# prevents mamba-admin start/stop
+OLD_PIDFILE=$WORKDIR/twistd.pid
+WARNING=/var/local/USE_INIT_SCRIPT_INSTEAD
+[ -f $OLD_PIDFILE ] && 
+    [ ! -L $OLD_PIDFILE ] && 
+        grep -q -P '^\d+$' $OLD_PIDFILE && 
+           die $OLD_PIDFILE exists
+echo "Use /etc/init.d/$APPNAME $@ instead." >$WARNING
+chmod 444 $WARNING
+ln -sf $WARNING $OLD_PIDFILE
